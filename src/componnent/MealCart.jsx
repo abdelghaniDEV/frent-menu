683a1c217457d1b1b42f07f661f740e39c2ae31b
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2024-11-19 17:01:53 +0100

    test

--- src/componnent/MealCart.jsx
+++ src/componnent/MealCart.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "motion/react";
+import MealComponent from "./Meal";
 
 function MealCart({ product, setProduct }) {
+  const [oppenImage, setOpenImage] = useState(false);
   return (
     <div className="" onClick={() => setProduct(product)}>
       <div className="border-b-[1px] items-center bg-[#373632] md:bg-inherit md:py-5   hover:bg-[#373632] cursor-pointer ">
@@ -27,7 +29,10 @@ function MealCart({ product, setProduct }) {
               {product.price}
             </span>
           </div>
-          <div className="relative md:hidden w-[30%] border-l-[1px] ">
+          <div
+            className="relative md:hidden w-[30%] border-l-[1px] "
+            onClick={() => setOpenImage(true)}
+          >
             <motion.img
               src={product.image}
               className="  h-[100px] "
@@ -35,13 +40,29 @@ function MealCart({ product, setProduct }) {
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut" }}
             />
-          
           </div>
         </div>
         <div className="px-3 py-1 hidden">
           <p className="text-[13px]">{product.description}</p>
         </div>
       </div>
+      {oppenImage && (
+        <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full h-full  bg-[rgba(0,0,0,0.64)] z-50">
+        
+          <img src={product?.image} className="h-[350px]" />
+          <div className="flex flex-col items-center text-center px-2 border-[1px] mx-5 relative  ">
+            <h1 className="text-[30px] font-[500]">{product?.name}</h1>
+            <p >{product?.description}</p>
+            <h2 className="flex items-center gap-2 text-[30px] font-[500]">
+              <i className="bx bx-shekel"></i>
+              <span>{product?.price}</span>
+            </h2>
+            <i onClick={() => setOpenImage(false)} className="bx bx-x text-[30px] fixed top-0 left-0 bg-[black] border-[1px] rounded-[20px] mx-5 mt-5"></i>
+          </div>
+
+          
+        </div>
+      )}
     </div>
   );
 }
