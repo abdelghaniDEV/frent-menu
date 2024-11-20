a541340532e3738de93345ee32270bc512588187
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2024-11-20 14:06:57 +0100

    test

--- src/componnent/Footer.jsx
+++ src/componnent/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className=' md:py-2 md:border-[1px] mt-6   '>
-        <div className='flex items-center justify-center cls bottom-0 '>
-        <a href='https://www.instagram.com/fabluxgroup?igsh=YnpxbWgyemE1cDN6' className='text-center'>Create By fablux group</a>
+        <div className='flex items-center justify-center cls pb-3 '>
+        <a href='https://www.instagram.com/fabluxgroup?igsh=YnpxbWgyemE1cDN6' className='text-center text-[15px]'>Create By fablux group</a>
         </div>
     </div>
   )
